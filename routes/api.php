c0ef45ca346ee704b5e47469c72cf03a879ed30b
Jason Moy <myolwinmoeaung125@gmail.com>
2025-07-18 15:34:39 -0700

    INitial Backend with auth and jobs api

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
